888e7c038cdf9bbcd8d9fbea1baed1ad321119c1
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-14 01:10:33 +0000

    naglagay ako ng comment

--- act1.java
+++ act1.java
@@ -1,6 +1,13 @@
 public class act1 {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        // This is a single line comment
+        /* THis
+         * is  
+         * a
+         * multi-line
+         * comment
+         */
     }
     
 }
