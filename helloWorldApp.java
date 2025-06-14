9e30abc473f5a233f506e894b2cc6d8d4fc3558e
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-14 07:28:34 +0000

    change Filename or Rename

--- helloWorldApp.java
+++ helloWorldApp.java
@@ -2,7 +2,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-public class try1 {
+public class helloWorldApp {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // Display the string.
     }
