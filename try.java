3eeb4f0a8a15697c704cff7a749ba0bfda94652e
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-14 07:11:14 +0000

    FIX THE ERROR AND CHANGE THE OUT PUT TO KAMUSTA MUNDO!

--- try.java
+++ try.java
@@ -4,6 +4,6 @@
  */
 public class try {
     public static void main(String[] args) {
-        System.out.println("Hello World"); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
