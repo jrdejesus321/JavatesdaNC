88527f78f1eda83e68dc1c6e4766eabf715ac2af
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-20 17:59:26 +0000

    added population

--- act2.java
+++ act2.java
@@ -8,13 +8,14 @@ public class act2{
     String nigger =" nigger 1 ";
     String nigger2 = "nigger 2 ";
     String BBC = nigger + nigger2;
-    act2 norights = new act2(); 
-    norights.name = " black people";
-    java.util.Date today = new java.util.Date();
-    String greeting = "Kamusta nigga";
-    System.out.println(" Kamusta Niiga"  + args[1] + BBC);
-    System.out.println("Under the section of 017 people that have no rights are:" + norights.name);
-    
+    int numpopulation = 100_000;
+        act2 norights = new act2(); 
+            norights.name = " black people";
+        java.util.Date today = new java.util.Date();
+            String greeting = "Kamusta nigga";
+                System.out.println(" Kamusta Niiga"  + args[1] + BBC);
+                System.out.println("Under the section of 017 people that have no rights are:" + norights.name);
+                System.out.println("The number of their Population is: " + numpopulation +" Thousand Blacks");
     } 
 
   
