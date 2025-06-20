e15d4b252403570366e09dc1caa75c10749a8d3e
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-20 17:55:15 +0000

    adding a rule

--- act2.java
+++ act2.java
@@ -1,15 +1,21 @@
 //referece//
 
 public class act2{ 
-
+    String name;
+    
 
     public static void main(String[] args) {
     String nigger =" nigger 1 ";
     String nigger2 = "nigger 2 ";
-    String BBC = nigger + nigger2; 
+    String BBC = nigger + nigger2;
+    act2 norights = new act2(); 
+    norights.name = " black people";
     java.util.Date today = new java.util.Date();
     String greeting = "Kamusta nigga";
     System.out.println(" Kamusta Niiga"  + args[1] + BBC);
+    System.out.println("Under the section of 017 people that have no rights are:" + norights.name);
     
     } 
+
+  
 }
