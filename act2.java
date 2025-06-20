80e5221a8b6aa16f3bbf65e0418d0c1b19c3f7fc
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-20 16:51:09 +0000

    Creating a nigga

--- act2.java
+++ act2.java
@@ -1,1 +1,10 @@
+public class act2{
 
+
+    public static void main(String[] args) {
+    java.util.Date today = new java.util.Date();
+    String greeting = "Kamusta nigga";
+    System.out.println("Kamusta Niiga"  + args[1]);
+
+    } 
+}
