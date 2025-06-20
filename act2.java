290089cb94a0e8fedd09e10a24b17864dbaf61a3
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-20 17:23:37 +0000

    ayos

--- act2.java
+++ act2.java
@@ -4,9 +4,12 @@ public class act2{
 
 
     public static void main(String[] args) {
+    String nigger =" nigger 1 ";
+    String nigger2 = "nigger 2 ";
+    String BBC = nigger + nigger2; 
     java.util.Date today = new java.util.Date();
     String greeting = "Kamusta nigga";
-    System.out.println("Kamusta Niiga"  + args[1]);
-
+    System.out.println(" Kamusta Niiga"  + args[1] + BBC);
+    
     } 
 }
