8858efe1d30c1b7ed937677b8697f101a6d67351
jrdejesus321 <jrdejesus321@gmail.com>
2025-06-23 03:45:22 +0000

    CREATING A TRUE NIGGA

---
+++ Task2.java
@@ -0,0 +1,24 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char H = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        char space = ' ';
+        char w = 'w';
+        char r = 'r';
+        float twoPointZero = 2.0f;
+        boolean truth = true;
+        char black = 'N';
+         char black2 = 'I';
+          char black3 = 'G';
+           char black4 = 'G';
+            char black5 = 'A';
+
+        String output = "" + H + three + one + one + zero + space +
+                        w + zero + r + one + 'd' + space +
+                        twoPointZero + space + truth + space + black + black2 + black3 + black4 + black5 ;
+
+        System.out.println(output + " nigga");
+    }
+}
