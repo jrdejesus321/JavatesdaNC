4b1ae3c9f9bc226f204cd8b7bf5d7b8b3bdc9162
jrdejesus321 <jrdejesus321@gmail.com>
2025-07-26 22:49:09 +0800

    Update task3.java

--- task3.java
+++ task3.java
@@ -1,1 +1,18 @@
+public class task3 {
+    public static void main(String[] args) {
+        String a = new String("Wow");
+        String c = "";
+        String b = a;
+        String d = b + "!";
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+
+    }
+}
 
